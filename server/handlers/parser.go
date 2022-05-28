295f337d3d9f65b7840b8095cb81a70c04ca978c
Ezhov Artem <ejow-artem@yandex.ru>
2022-05-29 04:30:47 +0700

    написал парсер GET параметров для координат, id и имени города, а также zip кода

---
+++ server/handlers/parser.go
@@ -0,0 +1,70 @@
+package handlers
+
+import (
+	"errors"
+	"net/url"
+	"strconv"
+)
+
+type parser struct{}
+
+func (p parser) parseCoordinates(query url.Values) (lat, lon float64, err error) {
+	if query.Get("lat") == "" {
+		err = errors.New("empty latitude")
+		return
+	}
+
+	if query.Get("lon") == "" {
+		err = errors.New("empty longitude")
+		return
+	}
+
+	if lat, err = strconv.ParseFloat(query.Get("lat"), 64); err != nil {
+		err = errors.New("invalid latitude")
+		return
+	}
+
+	if lon, err = strconv.ParseFloat(query.Get("lon"), 64); err != nil {
+		err = errors.New("invalid longitude")
+		return
+	}
+
+	return
+}
+
+func (p parser) parseCityName(query url.Values) (q string, err error) {
+	if q = query.Get("q"); q == "" {
+		err = errors.New("empty q")
+		return
+	}
+
+	return
+}
+
+func (p parser) parseCityID(query url.Values) (id int64, err error) {
+	if query.Get("id") == "" {
+		err = errors.New("empty id")
+		return
+	}
+
+	if id, err = strconv.ParseInt(query.Get("id"), 10, 64); err != nil {
+		err = errors.New("invalid id")
+		return
+	}
+
+	return
+}
+
+func (p parser) parseZipCode(query url.Values) (zip int64, err error) {
+	if query.Get("zip") == "" {
+		err = errors.New("empty zip")
+		return
+	}
+
+	if zip, err = strconv.ParseInt(query.Get("zip"), 10, 64); err != nil {
+		err = errors.New("invalid zip")
+		return
+	}
+
+	return
+}
---
+++ server/handlers/parser_test.go
@@ -0,0 +1,206 @@
+package handlers
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestParseCoordinates(t *testing.T) {
+	data := []struct {
+		name   string
+		query  url.Values
+		errMsg string
+	}{
+		{
+			"latitude is empty",
+			map[string][]string{
+				"lat": {""},
+				"lon": {""},
+			},
+			"empty latitude",
+		},
+		{
+			"longitude is empty",
+			map[string][]string{
+				"lat": {"10"},
+				"lon": {""},
+			},
+			"empty longitude",
+		},
+		{
+			"latitude is invalid",
+			map[string][]string{
+				"lat": {"ttt"},
+				"lon": {"10"},
+			},
+			"invalid latitude",
+		},
+		{
+			"longitude is invalid",
+			map[string][]string{
+				"lat": {"10"},
+				"lon": {"ttt"},
+			},
+			"invalid longitude",
+		},
+		{
+			"successful test",
+			map[string][]string{
+				"lat": {"10"},
+				"lon": {"10"},
+			},
+			"",
+		},
+	}
+
+	p := parser{}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			_, _, err := p.parseCoordinates(d.query)
+
+			var errMsg string
+			if err != nil {
+				errMsg = err.Error()
+			}
+
+			if errMsg != d.errMsg {
+				t.Errorf("Expected error `%s`, got `%s`", d.errMsg, errMsg)
+			}
+		})
+	}
+}
+
+func TestParseCityName(t *testing.T) {
+	data := []struct {
+		name   string
+		query  url.Values
+		errMsg string
+	}{
+		{
+			"city name is empty",
+			map[string][]string{
+				"q": {""},
+			},
+			"empty q",
+		},
+		{
+			"successful test",
+			map[string][]string{
+				"q": {"London"},
+			},
+			"",
+		},
+	}
+
+	p := parser{}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			_, err := p.parseCityName(d.query)
+
+			var errMsg string
+			if err != nil {
+				errMsg = err.Error()
+			}
+
+			if errMsg != d.errMsg {
+				t.Errorf("Expected error `%s`, got `%s`", d.errMsg, errMsg)
+			}
+		})
+	}
+}
+
+func TestParseCityID(t *testing.T) {
+	data := []struct {
+		name   string
+		query  url.Values
+		errMsg string
+	}{
+		{
+			"city id is empty",
+			map[string][]string{
+				"id": {""},
+			},
+			"empty id",
+		},
+		{
+			"city id is invalid",
+			map[string][]string{
+				"id": {"ttt"},
+			},
+			"invalid id",
+		},
+		{
+			"successful test",
+			map[string][]string{
+				"id": {"10"},
+			},
+			"",
+		},
+	}
+
+	p := parser{}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			_, err := p.parseCityID(d.query)
+
+			var errMsg string
+			if err != nil {
+				errMsg = err.Error()
+			}
+
+			if errMsg != d.errMsg {
+				t.Errorf("Expected error `%s`, got `%s`", d.errMsg, errMsg)
+			}
+		})
+	}
+}
+
+func TestZipCode(t *testing.T) {
+	data := []struct {
+		name   string
+		query  url.Values
+		errMsg string
+	}{
+		{
+			"zip is empty",
+			map[string][]string{
+				"zip": {""},
+			},
+			"empty zip",
+		},
+		{
+			"zip is invalid",
+			map[string][]string{
+				"zip": {"ttt"},
+			},
+			"invalid zip",
+		},
+		{
+			"successful test",
+			map[string][]string{
+				"zip": {"10"},
+			},
+			"",
+		},
+	}
+
+	p := parser{}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			_, err := p.parseZipCode(d.query)
+
+			var errMsg string
+			if err != nil {
+				errMsg = err.Error()
+			}
+
+			if errMsg != d.errMsg {
+				t.Errorf("Expected error `%s`, got `%s`", d.errMsg, errMsg)
+			}
+		})
+	}
+}
