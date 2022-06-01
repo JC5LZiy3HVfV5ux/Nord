4205e37fca288676aa4906ebe04ef84065a41667
Ezhov Artem <ejow-artem@yandex.ru>
2022-06-02 00:07:42 +0700

    добавил метод для извлечения http status code из возвращаемой ошибки

--- server/handlers/response.go
+++ server/handlers/response.go
@@ -2,25 +2,28 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/JC5LZiy3HVfV5ux/openweather-cache-server/openweather"
 )
 
 type response struct{}
 
 func (r response) httpErrorResponse(w http.ResponseWriter, code int, err error) {
-	body := fmt.Sprintf(`{"cod":%d,"message":"%s"}`, code, err.Error())
+	response := fmt.Sprintf(`{"cod":%d,"message":"%s"}`, code, err.Error())
 	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(code)
-	if _, err := w.Write([]byte(body)); err != nil {
+	if _, err := w.Write([]byte(response)); err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
 
-func (r response) httpJsonResponse(w http.ResponseWriter, code int, model interface{}) {
+func (r response) httpResponse(w http.ResponseWriter, code int, model interface{}) {
 	response, err := json.Marshal(model)
 	if err != nil {
 		log.Println(err)
@@ -34,4 +37,12 @@ func (r response) httpJsonResponse(w http.ResponseWriter, code int, model interf
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+}
+
+func (r response) getStatusCode(err error) int {
+	var openweatherError *openweather.HttpError
+	if errors.As(err, &openweatherError) {
+		return int(openweatherError.Cod)
+	}
+	return http.StatusInternalServerError
 }
